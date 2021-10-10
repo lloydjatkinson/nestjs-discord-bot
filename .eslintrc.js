ece469738c7014a690c74352588491d64ea5ab43
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2021-10-10 12:16:32 +0000

    feature: Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    // 'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -20,10 +20,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-
-    indent: ['error', 4],
-    'linebreak-style': 'off',
-    'eol-last': ['error', 'never'],
-    'max-len': ['error', { code: 450 }],
   },
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FactsModule } from './facts/facts.module';
-import { ScheduleModule } from '@nestjs/schedule';
-import { CommandsModule } from './commands/commands.module';
-import { FeaturesModule } from './features/features.module';
-import { CommandRegistrationModule } from './command-registration/command-registration.module';
-import { CommandRegistrationModule } from './command-registration/command-registration.module';
-import { CommandsModule } from './commands/commands.module';
-import { CommandRegistrationService } from './command-registration/command-registration.service';
-import { CommandsModule } from './commands/commands.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV || 'development'}` }),
-        ScheduleModule.forRoot(),
-        DiscordClientModule,
-        FactsModule,
-        CommandsModule,
-        CommandRegistrationModule,
-        FeaturesModule,
-    ],
-    providers: [AppService, CommandRegistrationService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
