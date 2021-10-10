9bf8ed8ae79fe2cc40d8887feb4406c8afad06c3
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2021-10-10 14:44:59 +0000

    feature: Add empty configuration files

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { DiscordClientService } from './discord-client/discord-client.service';
 import { CommonCommandsModule } from './common-commands/common-commands.module';
 import { CommonQueriesModule } from './common-queries/common-queries.module';
 
 @Module({
     providers: [DiscordClientService],
-    imports: [CommonCommandsModule, CommonQueriesModule]
+    imports: [ConfigModule, CommonCommandsModule, CommonQueriesModule]
 })
 export class DiscordModule {}
